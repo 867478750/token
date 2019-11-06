0d44e71b9214c866ba1180c585758bc0a0b0f48f
nlb <15624977344@163.com>
2019-11-06 18:49:16 +0800

    第二次

--- src/main/java/com/nlb/auth2/app/config/Oauth2Config.java
+++ src/main/java/com/nlb/auth2/app/config/Oauth2Config.java
@@ -3,17 +3,19 @@ package com.nlb.auth2.app.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.security.KeyPair;
 
 @Configuration
 public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
@@ -21,7 +23,7 @@ public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    private RedisConnectionFactory redisConnectionFactory;
+    private RedisConnectionFactory  redisConnectionFactory;
 
 
     @Override//配置客户端
@@ -40,11 +42,6 @@ public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
         super.configure(clients);
     }
 
-    @Override//配置token怎么存储
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore());
-        super.configure(endpoints);
-    }
 
     @Override//配置token的访问权限
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -53,7 +50,29 @@ public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
     }
 
 
+    @Override//配置token怎么存储
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore()).accessTokenConverter(jwtAccessTokenConverter);
+        super.configure(endpoints);
+    }
+
+    @Autowired
+    JwtAccessTokenConverter jwtAccessTokenConverter;
+
     private TokenStore tokenStore(){
-        return new RedisTokenStore(redisConnectionFactory);
+      //  return new RedisTokenStore(redisConnectionFactory);
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+//        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+//        jwtAccessTokenConverter.setSigningKey("nlb");
+//        return jwtAccessTokenConverter;
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        ClassPathResource resource = new ClassPathResource("static/jwt.jks");
+        KeyStoreKeyFactory keyFactory = new KeyStoreKeyFactory(resource,"nlb123".toCharArray());
+        KeyPair keyPair = keyFactory.getKeyPair("nlb");
+        jwtAccessTokenConverter.setKeyPair(keyPair);
+        return jwtAccessTokenConverter;
     }
 }
--- src/main/java/com/nlb/auth2/app/controller/UserController2.java
+++ src/main/java/com/nlb/auth2/app/controller/UserController2.java
@@ -1,5 +1,7 @@
 package com.nlb.auth2.app.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,5 +12,12 @@ public class UserController2 {
     @GetMapping("/test2")
     public String test02(){
         return "test2";
+    }
+
+    @ResponseBody
+    @GetMapping("/info")
+    public Object getInfo(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication;
     }
 }
